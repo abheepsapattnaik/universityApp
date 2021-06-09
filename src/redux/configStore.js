88171929a6c8ca7d68f4907c199f6ec2b96bed75
Abheepsa Pattnaik <abheepsapattnaik@gmail.com>
2021-06-09 16:13:04 +0530

    #000 | Abheepsa | Use redux-thunk instead of devtool-extensions

--- src/redux/configStore.js
+++ src/redux/configStore.js
@@ -1,17 +1,14 @@
 import rootReducer from './rootReducer';
 import {applyMiddleware, createStore} from 'redux';
-import {createLogger} from 'redux-logger';
 import {composeWithDevTools} from 'redux-devtools-extension';
-import {intercept} from './interceptMiddleware';
+import thunk from "redux-thunk";
 
 export default function configStore() {
     let storeEnhancer = undefined;
     if (process.env.NODE_ENV !== 'PRODUCTION') {
         storeEnhancer = composeWithDevTools(
-            applyMiddleware(
-                createLogger({collapsed: true}),
-                intercept
-            ))
+            applyMiddleware(thunk)
+        )
     }
 
     return createStore(rootReducer, storeEnhancer);
